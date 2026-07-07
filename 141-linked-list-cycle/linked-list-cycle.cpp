1e1d30f26fdd5544198c788854908f586de8536e
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-07-07 09:54:27 +0530

    Time: 8 ms (83.33%) | Memory: 12 MB (23.23%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,18 +11,34 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* current = head;
      
-        unordered_set<ListNode*>value;
-        while(current!=nullptr){
-            if(value.contains(current)){
-                return true;
+        // unordered_set<ListNode*>value;
+        // while(current!=nullptr){
+        //     if(value.contains(current)){
+        //         return true;
                 
-            }
-            value.insert(current);
-            current = current->next;
+        //     }
+        //     value.insert(current);
+        //     current = current->next;
             
+        // }
+        // return false;
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast =  fast->next->next;
+
+
+            if(fast == slow){
+                return true;
+            }
+            
+            
+
         }
         return false;
-
         
     }
 };
