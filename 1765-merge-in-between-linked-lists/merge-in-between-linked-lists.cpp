086cae539065b2173b54681e6967edcbfa697584
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-07-10 18:20:27 +0530

    Time: 201 ms (51.74%) | Memory: 99.5 MB (56.62%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        int count = -1;
+        ListNode* current = list1;
+        ListNode* cur = list2;
+        ListNode* last = list1;
+        while(current->next!=nullptr){
+            count++;
+            if(count == b){
+                last = current->next;
+                break;
+            }
+            current = current->next;
+        }
+        count = -1;
+       
+        current= list1;
+
+        while(current!=nullptr&&current->next!=nullptr){
+            count++;
+           
+            if(count == b){
+                current->next = nullptr;
+            }
+            current = current->next;
+        }
+        count =-1;
+
+        current = list1;
+        while(current->next!=nullptr){
+            count++;
+           
+            if(count == a-1){
+                current->next = cur;
+                break;
+            }
+            else
+                current = current->next;
+        }
+        while(cur->next!=nullptr){
+            cur = cur ->next;
+        }
+        cur -> next = last;
+
+        return list1;
+        
+    }
+};
