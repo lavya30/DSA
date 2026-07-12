e766d6b0bc88b2fac87ad5ba3f793552142f5f34
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-07-12 21:19:04 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (39.88%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,43 +1,60 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        if(nums.size()==1){
-            return {nums[nums.size()-1]*nums[nums.size()-1]};
+        
+        vector<int>result(nums.size());
+        int left = 0;
+        int right = nums.size()-1;
+
+        for(int i=nums.size()-1;i>=0;i--){
+
+            int leftvalue = nums[left]*nums[left];
+            int rightvalue = nums[right]*nums[right];
+
+            if(leftvalue>rightvalue){
+                result[i] = leftvalue;
+                left++;
+            }
+            else{
+                result[i] = rightvalue;
+                right--;
+            }
+
         }
        
-        vector<int>neg;
-        vector<int>pos;
-        vector<int>result;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0){
-                neg.push_back(nums[i]*nums[i]);
-            }
-            else{
-                pos.push_back(nums[i]*nums[i]);
-            }
+        // vector<int>neg;
+        // vector<int>pos;
+        // vector<int>result;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]<0){
+        //         neg.push_back(nums[i]*nums[i]);
+        //     }
+        //     else{
+        //         pos.push_back(nums[i]*nums[i]);
+        //     }
 
-        }
-        int i = neg.size()-1;
-        int j = 0;
+        // }
+        // int i = neg.size()-1;
+        // int j = 0;
 
-        while(i>=0 && j<pos.size()){
-            if(neg[i]<pos[j]){
-                result.push_back(neg[i]);
-                i--;
-            }
-            else{
-                result.push_back(pos[j]);
-                j++;
-            }
-        } 
-        while(i>=0){
-            result.push_back(neg[i]);
-            i--;
-        }
-        while(j<pos.size()){
-            result.push_back(pos[j]);
-            j++;
-        }
+        // while(i>=0 && j<pos.size()){
+        //     if(neg[i]<pos[j]){
+        //         result.push_back(neg[i]);
+        //         i--;
+        //     }
+        //     else{
+        //         result.push_back(pos[j]);
+        //         j++;
+        //     }
+        // } 
+        // while(i>=0){
+        //     result.push_back(neg[i]);
+        //     i--;
+        // }
+        // while(j<pos.size()){
+        //     result.push_back(pos[j]);
+        //     j++;
+        // }
         return result;
     }
 };
