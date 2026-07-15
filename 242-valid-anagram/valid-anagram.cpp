2cdcbdaa4fdf60bbd20672add58d639491cd8d5d
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-07-15 22:29:57 +0530

    Time: 3 ms (46.00%) | Memory: 9.8 MB (25.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char,int>hash1;
-        unordered_map<char,int>hash2;
+        if(size(s)!=size(t)){
+            return false;
+        }
         for(int i=0;i<size(s);i++){
             hash1[s[i]]++;
         }
          for(int i=0;i<size(t);i++){
-            hash2[t[i]]++;
+            hash1[t[i]]--;
         }
-        return hash1==hash2;
+        for(auto it:hash1){
+            if(it.second>0){
+                return false;
+            }
+        }
+        return true;
     }
 };
