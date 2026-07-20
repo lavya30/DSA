7ae7060be2c00ba4337e10e656ff8d8d403ce567
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-07-20 16:23:14 +0530

    Time: 3 ms (48.56%) | Memory: 28.2 MB (66.95%) - LeetSync

--- 4252-first-unique-even-element/first-unique-even-element.cpp
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int firstUniqueEven(vector<int>& nums) {
-        unordered_map<int,int>hash;
+        map<int ,int>hash;
+        
+        // unordered_map<int,int>hash;
         for(int i=0;i<nums.size();i++){
-            hash[nums[i]]++;
+            if(nums[i]%2==0)
+                hash[nums[i]]++;
         }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0){
-                if(hash[nums[i]] == 1){
-                    return nums[i];
-                }
+        for(int num:nums){
+            if(num%2==0 && hash[num]== 1){
+                return num;
             }
         }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]%2==0){
+        //         if(hash[nums[i]] == 1){
+        //             return nums[i];
+        //         }
+        //     }
+        // }
 
         return -1;
 
