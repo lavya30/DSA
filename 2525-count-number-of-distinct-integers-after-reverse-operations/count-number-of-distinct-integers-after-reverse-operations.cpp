4e69116adc86c9b55a5344cb5802e5ff8081b2d8
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-05 16:20:48 +0530

    Time: 361 ms (8.17%) | Memory: 113.5 MB (38.45%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,31 +1,38 @@
 class Solution {
 private:
     int reverseNum(int n){
+        while(n>0 && n%10==0)
+            n/=10;
         int rev = 0;
         while(n!=0){
             int digit = n%10;
             rev = (rev*10)+digit;
             n/=10;
         }
-        string s = to_string(rev);
-        for(int i = 0;i<size(s);i++){
-            if(s[i]==0){
-                  s.erase(s.begin() + i); 
-            }
-            else
-                break;
-        }
-        return stoi(s);
+        return rev;
+//         string s = to_string(rev);
+//         for(int i = 0;i<size(s);i++){
+//             if(s[i]==0){
+//                   s.erase(s.begin() + i); 
+//             }
+//             else
+//                 break;
+//         }
+//         return stoi(s);
     }
 public:
     int countDistinctIntegers(vector<int>& nums) {
         set<int>ans(nums.begin(),nums.end());
-
-        for(int i = 0;i<nums.size();i++){
-
+        for(int i= 0;i<nums.size();i++){
             int val = reverseNum(nums[i]);
             ans.insert(val);
         }
+
+        // for(int i = 0;i<nums.size();i++){
+
+        //     int val = reverseNum(nums[i]);
+        //     ans.insert(val);
+        // }
         return ans.size();
         
     }
