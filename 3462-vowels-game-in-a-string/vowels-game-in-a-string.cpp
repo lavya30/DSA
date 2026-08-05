fe9a506c37aa8a1803a03dd95c1f0ef237040066
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-05 16:58:01 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (96.20%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        int vowels = 0;
         for(int i= 0;i<size(s);i++){
             if(s[i]=='a' || s[i]=='i' || s[i] == 'o' || s[i] == 'e' || s[i] == 'u')
-                break;
-            if(i==size(s)-1)
-                return false;
-        }
-        for(int i = 0; i<size(s);i++){
-              if(s[i]=='a' || s[i]=='i' || s[i] == 'o' || s[i] == 'e' || s[i] == 'u')
-                vowels++;
-
-        }
-        bool alice = true;
-        bool bob =  false;
-        while(vowels!=0){
-          
-            if(vowels %2!= 0 && alice == true)
                 return true;
-            else if(vowels%2 == 0 && bob ==true)    
-                return false;
-
-            else if(vowels%2==0 && alice == true ){
-                vowels = vowels - (vowels-1);
-                alice = false;
-                bob = true;
-            }
-            else if(vowels%2!=0 && bob == true){
-                alice = true;
-
-                bob = false;
-            }
 
         }
-        return alice;
+        return false;
+        // int vowels = 0;
+        // for(int i= 0;i<size(s);i++){
+        //     if(s[i]=='a' || s[i]=='i' || s[i] == 'o' || s[i] == 'e' || s[i] == 'u')
+        //         break;
+        //     if(i==size(s)-1)
+        //         return false;
+        // }
+        // for(int i = 0; i<size(s);i++){
+        //       if(s[i]=='a' || s[i]=='i' || s[i] == 'o' || s[i] == 'e' || s[i] == 'u')
+        //         vowels++;
+
+        // }
+        // bool alice = true;
+        // bool bob =  false;
+        // while(vowels!=0){
+          
+        //     if(vowels %2!= 0 && alice == true)
+        //         return true;
+        //     else if(vowels%2 == 0 && bob ==true)    
+        //         return false;
+
+        //     else if(vowels%2==0 && alice == true ){
+        //         vowels = vowels - (vowels-1);
+        //         alice = false;
+        //         bob = true;
+        //     }
+        //     else if(vowels%2!=0 && bob == true){
+        //         alice = true;
+
+        //         bob = false;
+        //     }
+
+        // }
+        // return alice;
     }
 
 };
