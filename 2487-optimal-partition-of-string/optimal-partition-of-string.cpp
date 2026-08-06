21cf2de3bac0c53c4b47296373f412a29ebc8898
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-06 18:40:47 +0530

    Time: 1 ms (89.60%) | Memory: 13 MB (78.16%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int count = 0;
-        char t ='#' ;
+        int counter = 0;
+        vector<int>freq(26,0);
+        for(auto ch:s){
+            if(freq[ch-'a']>0){
+                counter++;
+                fill(freq.begin(),freq.end(),0);
+            }
+            
+            freq[ch - 'a']++;
+          
+        }
+        // int count = 0;
+        // char t ='#' ;
 
-        vector<char>temp;
-        for(int i= 0;i<size(s);i++){
+        // vector<char>temp;
+        // for(int i= 0;i<size(s);i++){
 
             
-            temp.push_back(t);
+        //     temp.push_back(t);
 
-            if(find(temp.begin(),temp.end(),s[i]) == temp.end()){
-                temp.push_back(s[i]);
-            }
-            else{
-                t = s[i];
-                count++;
-                temp.clear();
-            }
-        }
-        return count+1;
+        //     if(find(temp.begin(),temp.end(),s[i]) == temp.end()){
+        //         temp.push_back(s[i]);
+        //     }
+        //     else{
+        //         t = s[i];
+        //         count++;
+        //         temp.clear();
+        //     }
+        // }
+        // return count+1;
+        return counter+1;
     }
 };
