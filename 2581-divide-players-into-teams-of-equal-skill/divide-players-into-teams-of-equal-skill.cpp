37c3d9c6ac90e039fde6ae75c5383f800c2f5dd2
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-06 19:13:04 +0530

    Time: 16 ms (40.91%) | Memory: 56.4 MB (84.00%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        int score = 0;
+
         sort(skill.begin(),skill.end());
-        vector<pair<int,int>>temp;
         int low = 0;
         int high = skill.size()-1;
+        int target = skill[low]+skill[high];
+        long long ans = 0;
         while(low<high){
-            
-            temp.push_back({skill[low],skill[high]});
-            low++;
-            high--;
-
-        }
-        for(int i= 0;i<temp.size()-1;i++){
-            if(temp[i].second+ temp[i].first != temp[i+1].second+temp[i+1].first){
+            if(skill[low]+skill[high] != target){
                 return -1;
             }
-        }
-        long long ans = 0;
-        for(auto it:temp){
-            ans+=it.first * it.second;
-        }
-        return ans;
+            ans+= skill[low]*skill[high];
+            low++;
+            high--;
+    }
+    return ans;
+
+
+        // int score = 0;
+        // sort(skill.begin(),skill.end());
+        // vector<pair<int,int>>temp;
+        // int low = 0;
+        // int high = skill.size()-1;
+        // while(low<high){
+            
+        //     temp.push_back({skill[low],skill[high]});
+        //     low++;
+        //     high--;
+
+        // }
+        // for(int i= 0;i<temp.size()-1;i++){
+        //     if(temp[i].second+ temp[i].first != temp[i+1].second+temp[i+1].first){
+        //         return -1;
+        //     }
+        // }
+        // long long ans = 0;
+        // for(auto it:temp){
+        //     ans+=it.first * it.second;
+        // }
+        // return ans;
         
     }
 };
