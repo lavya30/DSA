55cb42ef3081e3c89b5a1d5221241e424655211c
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-07 18:16:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (22.62%) - LeetSync

--- 3830-find-closest-person/find-closest-person.cpp
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -2,22 +2,32 @@ class Solution {
 public:
     int findClosest(int x, int y, int z) {
 
+        int val1 = abs(z-x);
+        int val2 =  abs(z-y);
 
-        while(x!=z && y!=z){
-            if(x<z)
-                x++;
-            else
-                x--;
-            if(y<z)
-                y++;
-            else 
-                y--;
-        }
-        if(x ==  z && y ==z) 
-            return 0;
-        else if(x == z)
+        if(val1<val2)
             return 1;
-        return 2;
+        else if(val2<val1)
+            return 2;
+        else return 0;
+
+
+
+        // while(x!=z && y!=z){
+        //     if(x<z)
+        //         x++;
+        //     else
+        //         x--;
+        //     if(y<z)
+        //         y++;
+        //     else 
+        //         y--;
+        // }
+        // if(x ==  z && y ==z) 
+        //     return 0;
+        // else if(x == z)
+        //     return 1;
+        // return 2;
         
     }
 };
