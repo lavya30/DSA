8c1654d375f8c6ed344061973a4a89146e82d6c0
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-07 21:37:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (81.86%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int findPermutationDifference(string s, string t) {
-        unordered_map<char,int>hash1;
-        for(int i = 0;i<size(s);i++){
-            hash1[s[i]] = i;
-        }
         int sum = 0;
-        for(int i = 0;i<size(t);i++){
-            int index = hash1[t[i]];
-            sum+=abs(i-index); 
+        for(int i = 0;i<size(s);i++){
+            char ch =  s[i];
+            int index = t.find(ch);
+            sum+=abs(index - i);
+            
         }
+
+        // unordered_map<char,int>hash1;
+        // for(int i = 0;i<size(s);i++){
+        //     hash1[s[i]] = i;
+        // }
+        // int sum = 0;
+        // for(int i = 0;i<size(t);i++){
+        //     int index = hash1[t[i]];
+        //     sum+=abs(i-index); 
+        // }
         return sum;
         
     }
