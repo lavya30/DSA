9b3844fe1888c7672cf469a45530d41d6ded592d
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-07 10:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 34.9 MB (58.92%) - LeetSync

--- 3533-snake-in-matrix/snake-in-matrix.cpp
+++ 3533-snake-in-matrix/snake-in-matrix.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
     int finalPositionOfSnake(int n, vector<string>& commands) {
+        int ans = 0;
+        for(int i = 0;i<commands.size();i++){
+            if(commands[i] == "RIGHT"){
+                ans+=1;
+            }
+            else if(commands[i]== "DOWN"){
+                ans+=n;
+            }
+            else if(commands[i]== "LEFT"){
+                ans-=1;
+            }
+            else if(commands[i] == "UP"){
+                ans-=n;
+            }
+        }
 
-        vector<vector<int>> grid(n, vector<int>(n));
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                grid[i][j] = (i*n)+j;
-           }
-       }
-       int ans = 0;
-       int row = 0;
-       int column = 0;
+        // vector<vector<int>> grid(n, vector<int>(n));
 
-       for(int i = 0;i<commands.size();i++){
-        if(commands[i] == "RIGHT"){
-            column++;
-            ans = grid[row][column];
-        }
-        else if(commands[i] == "LEFT"){
-            column--;
-            ans = grid[row][column];
-        }
-        else if(commands[i] == "UP"){
-            row--;
-            ans= grid[row][column];
-        }
-        else if(commands[i] == "DOWN"){
-            row++;
-            ans =  grid[row][column];
-        }
-       }
+    //     for(int i = 0;i<n;i++){
+    //         for(int j = 0;j<n;j++){
+    //             grid[i][j] = (i*n)+j;
+    //        }
+    //    }
+    //    int ans = 0;
+    //    int row = 0;
+    //    int column = 0;
+
+    //    for(int i = 0;i<commands.size();i++){
+    //     if(commands[i] == "RIGHT"){
+    //         column++;
+    //         ans = grid[row][column];
+    //     }
+    //     else if(commands[i] == "LEFT"){
+    //         column--;
+    //         ans = grid[row][column];
+    //     }
+    //     else if(commands[i] == "UP"){
+    //         row--;
+    //         ans= grid[row][column];
+    //     }
+    //     else if(commands[i] == "DOWN"){
+    //         row++;
+    //         ans =  grid[row][column];
+    //     }
+    //    }
        return ans;
         
     }
