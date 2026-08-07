b5adbbef6f700eb60d73137b32f11b3b59abdfbb
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-07 18:42:07 +0530

    Time: 3 ms (85.17%) | Memory: 28.8 MB (19.28%) - LeetSync

--- 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.cpp
+++ 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.cpp
@@ -1,26 +1,27 @@
 class Solution {
 private:
     int findMax(int n){
+        string s = "";
         int maxi = 0;
         while(n!=0){
             int digit = n%10;
             maxi = max(maxi , digit);
             n/=10;
+            s+='1';
 
         }
-        return maxi;
+        
+        return maxi * stoi(s);
     }
 public:
     int sumOfEncryptedInt(vector<int>& nums) {
         int sum = 0;
         for(int i = 0; i< nums.size();i++){
             int maxi = findMax(nums[i]);
-            int digit = floor(log10(nums[i]))+1;
-            string s = "";
-            for(int j = 0;j<digit;j++){
-                s+=to_string(maxi);
-            }
-            sum+=stoi(s);
+     
+            sum+=maxi;
+           
+
 
         }
         return sum;
