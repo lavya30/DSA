c9b323170149e98799f60d91adf060e72156f2ef
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-07 08:48:41 +0530

    Time: 15 ms (61.82%) | Memory: 17.6 MB (42.64%) - LeetSync

--- 4313-password-strength/password-strength.cpp
+++ 4313-password-strength/password-strength.cpp
@@ -2,28 +2,52 @@ class Solution {
 public:
     int passwordStrength(string password) {
         int ans = 0;
-        unordered_set<int>temp;
+        unordered_map<char ,int>hash;
         for(int i =0;i<size(password);i++){
-            int ascii = int(password[i]);
-            
-            
-            if(find(temp.begin(),temp.end(),ascii) == temp.end()){
-                if(ascii>=48 && ascii<=57){
+            hash[password[i]]++;
+        }
+
+        for(auto it:hash){
+            int ascii = int(it.first);
+            if(ascii>=48 && ascii<=57){ //digits
                 ans+=3;
-                }
-                else if(ascii>=65 && ascii <=122 ){
-        
-                    if(ascii >=97 && ascii<=122)
-                        ans+=1;
-               
-                    else
-                        ans+=2;
-                    }
-                else
-                    ans+=5;
-                }
-                temp.insert(ascii);
+            } 
+            else if(ascii >=97 && ascii<=122){ //a-z
+                ans+=1;
             }
+            else if(ascii>=65 && ascii<=90){ //A-Z
+                ans+=2;
+            }
+            else   //special characters
+                ans+=5;
+        }
+
+
+
+
+
+        // unordered_set<int>temp;
+        // for(int i =0;i<size(password);i++){
+        //     int ascii = int(password[i]);
+            
+            
+        //     if(find(temp.begin(),temp.end(),ascii) == temp.end()){
+        //         if(ascii>=48 && ascii<=57){
+        //         ans+=3;
+        //         }
+        //         else if(ascii>=65 && ascii <=122 ){
+        
+        //             if(ascii >=97 && ascii<=122)
+        //                 ans+=1;
+               
+        //             else
+        //                 ans+=2;
+        //             }
+        //         else
+        //             ans+=5;
+        //         }
+        //         temp.insert(ascii);
+        //     }
             
         return ans;
     }
