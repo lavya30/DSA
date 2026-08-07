79f6a3095c6efd51a6c23e05ef62d647023a6423
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-07 08:31:37 +0530

    Time: 107 ms (5.08%) | Memory: 17.6 MB (42.64%) - LeetSync

---
+++ 4313-password-strength/password-strength.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int passwordStrength(string password) {
+        int ans = 0;
+        set<int>temp;
+        for(int i =0;i<size(password);i++){
+            int ascii = int(password[i]);
+            
+            
+            if(find(temp.begin(),temp.end(),ascii) == temp.end()){
+                if(ascii>=48 && ascii<=57){
+                ans+=3;
+                }
+                else if(ascii>=65 && ascii <=122 ){
+        
+                    if(ascii >=97 && ascii<=122)
+                        ans+=1;
+               
+                    else
+                        ans+=2;
+                    }
+                else
+                    ans+=5;
+                }
+                temp.insert(ascii);
+            }
+            
+        return ans;
+    }
+};
