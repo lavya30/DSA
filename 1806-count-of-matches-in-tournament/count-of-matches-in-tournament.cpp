2ab0256651a8ab2e335f5e742c28542e8296d0ba
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-09 18:24:51 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (37.20%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int sum = 0;
+        return n-1;
+        // int sum = 0;
 
-        while(n!=1){
-            if(n%2!=0){
-                sum+= (n - 1)/2;
-                n = n - ((n-1)/2);
+        // while(n!=1){
+        //     if(n%2!=0){
+        //         sum+= (n - 1)/2;
+        //         n = n - ((n-1)/2);
 
-            }
-            else{
-                sum+=n/2;
-                n = n/2;
+        //     }
+        //     else{
+        //         sum+=n/2;
+        //         n = n/2;
 
-            }
+        //     }
 
-        }
-        return sum;
+        // }
+        // return sum;
         
     }
 };
