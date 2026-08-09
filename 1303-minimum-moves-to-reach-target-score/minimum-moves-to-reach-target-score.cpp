2333470998848dfe2db6db7d1ab48bb2f5612255
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-09 07:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (67.14%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int minMoves(int target, int maxDoubles) {
-        if(maxDoubles == 0)
-            return target-1;
+        
         int counter = 0;
         while(target>1 ){
+            if(maxDoubles == 0){
+                counter+= target -1;
+                break;
+
+            }
             if(target%2==0 && maxDoubles!=0){
                 target/=2;
                 maxDoubles--;
