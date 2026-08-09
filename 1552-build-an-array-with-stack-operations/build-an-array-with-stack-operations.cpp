6f08963459b73018209751d6f0c789a6b8c0930e
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-09 08:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (87.88%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string>ans;
-        vector<int>temp;
 
-        for(int i =1; i<=n;i++){
-            if(find(target.begin(),target.end(),i) !=  target.end()){
-                temp.push_back(i);
+        vector<string>ans;
+
+        int high = 0;
+        for(int i= 1;i<=n && high < target.size();i++){
+            if(i == target[high]){
                 ans.push_back("Push");
-                if(temp == target)
-                    break;
+                high++;
             }
             else{
                 ans.push_back("Push");
                 ans.push_back("Pop");
             }
-
+            
 
         }
+        // vector<int>temp;
+        // for(int i =1; i<=n;i++){
+        //     if(find(target.begin(),target.end(),i) !=  target.end()){
+        //         temp.push_back(i);
+        //         ans.push_back("Push");
+        //         if(temp == target)
+        //             break;
+        //     }
+        //     else{
+        //         ans.push_back("Push");
+        //         ans.push_back("Pop");
+        //     }
+
+
+        // }
         return ans;
-        
+      
     }
 };
