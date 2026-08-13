48e3df825652219a96aedfef7f5b136fe918cc94
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-13 10:07:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (2.79%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         int counter = 0;
-        bitset<100>a(start);
-        bitset<100>b(goal);
+        bitset<30>a(start);
+        bitset<30>b(goal);
 
         string str1 = a.to_string();
         string str2 = b.to_string();
