b80aea4bc956ea6bf74a3c91561963cec943431c
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-13 11:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (86.68%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string str= "";
+        for(int i = 0;i<size(s);i++){
+            if(s[i] == 'i'){
+                reverse(str.begin(),str.end());
+            }
+            else
+                str+=s[i];
+        }
+        return str;
+        
+    }
+};
