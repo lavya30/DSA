d67ac84067b8dceefca36ad370f75739998c71e0
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-15 10:46:59 +0530

    Time: 4 ms (58.09%) | Memory: 36 MB (81.64%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -11,44 +11,17 @@
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        ListNode* current = head->next;
-        ListNode* prev = head;
 
-
-        while(current != nullptr){ 
+        ListNode* current = head;
+        while(nullptr != current && nullptr != current->next){
+            int value = gcd(current->val , current->next->val);
+            ListNode* newNode = new ListNode(value);
+           
+            newNode ->next = current->next;
+            current ->next = newNode;
             
-            int gcdValue = gcd(prev->val, current->val);
-            
-            ListNode* gcdNode = new ListNode(gcdValue);
-            prev->next = gcdNode;
-            gcdNode->next = current;
-            current = current->next;
-            prev = prev->next->next;
+            current  = current->next->next;
         }
-
-        // while(prev->next!=nullptr){
-        //     int val1=prev->val;
-        //     int val2=current->val;
-        //     int mini =  min(val1,val2);
-        //     int gcd = 0;
-        //     for(int i = mini;i>0;i--){
-        //         if(val1%i==0 && val2%i==0){
-        //             gcd = max(gcd,i);
-        //             break;
-        //         }
-
-        //     }
-        //     ListNode* gcdNode = new ListNode(gcd);
-        //     prev->next =  gcdNode;
-        //     gcdNode->next =  current;
-
-
-        //     current = current->next;
-        //     prev = prev->next->next;
-
-            
-
-        // }
         return head;
     }
 };
