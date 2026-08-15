9895c7d5a6d01e4421dce5f783107f7bb5ed60c3
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-08-15 17:50:08 +0530

    Time: 95 ms (65.13%) | Memory: 263.3 MB (76.28%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -11,42 +11,23 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> badNums(nums.begin(), nums.end());
+
+
+        ListNode* current = head;
         ListNode* dummy = new ListNode(-1);
         ListNode* tail = dummy;
-        // vector<int>vec;
-        ListNode* current = head;
-        while(current!=nullptr){
-            if(badNums.find(current->val)==badNums.end()){
-                tail->next = current;
-                tail = tail->next;
+
+        unordered_set<int> num(nums.begin(),nums.end());
+
+        while(nullptr != current){
+            if(num.find(current -> val) == num.end()){
+                tail -> next = current;
+                tail = tail -> next;
             }
-            
             current = current -> next;
         }
-
-        // for(int i =0;i<nums.size();i++){
-        //     vec.erase(remove(vec.begin(), vec.end(), nums[i]), vec.end());
-        // }
-
-        // current =  head;
-        // while(current!=nullptr){
-        //     if(find(vec.begin(),vec.end(),current->val)!=vec.end()){
-        //         tail->next = current;
-        //         tail = tail->next;
-                
-        //     }
-        //     current =  current->next;
-            
-            
-        // }
-        tail->next = nullptr;
-
-        return dummy->next;
-
-
-
-
+        tail -> next = nullptr;
+        return dummy ->next;
         
     }
 };
