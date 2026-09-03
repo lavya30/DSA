698b9cf69ccbe63927362734d3139fad5f319ec5
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-09-03 22:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (19.07%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        bool negative =  false;
-        int j =0;
+
+        int negative = false;
+
         int i = 0;
-        string s1 = "";
-        while(i<size(s) && s[i]==' '){
-            j++;
-            i++;
+
+        while(i<size(s) && s[i] == ' '){
+            
+
             if(i == size(s))
                 return 0;
+            i++;
         }
-        if(s[i]=='-'){
+        if(i<size(s) && s[i] == '-'){
+            negative  = true;
+            i++;
 
+        } 
+        else if(i<size(s) && s[i] == '+'){
             i++;
-            
-            negative  =  true;
-        }
-        else if(i<size(s) && s[i]=='+'){
             negative = false;
-            i++;
-            
+
         }
+
         long long res = 0;
-        for(int k = i;i<size(s);i++){
-            if(isdigit(s[i])){
-                int digit = s[i]-'0';
-                res = res*10+digit;
+
+        for(int j = i; j<size(s);j++){
+            if(isdigit(s[j])){
+                int num = s[j] - '0';
+                res = res*10 + num; 
             }
-            else 
+            else{
                 break;
-            if (negative && -res < INT_MIN) return INT_MIN;
-            if (!negative && res > INT_MAX) return INT_MAX;
-        }
+            }
+            if(negative && -res < INT_MIN) 
+                return INT_MIN;
+            else if(!negative && res > INT_MAX) 
+                return INT_MAX;
         
-        return negative ? -res : res;
+        }
+        return negative ? -res:res;
+        
+        
+        
+        
     }
 };
