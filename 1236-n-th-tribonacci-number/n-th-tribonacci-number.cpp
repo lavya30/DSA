e2a20ad695de573c880d1a8378dc78c668130c51
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-09-11 22:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.25%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -6,15 +6,27 @@ public:
         else if(n <2)
             return 1; 
     
-        vector<int>dp(n+1,-1);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        // vector<int>dp(n+1,-1);
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 1;
 
-        for(int i = 3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        // for(int i = 3;i<=n;i++){
+        //     dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        // }
+        // return dp[n];
+
+        int t1  = 0;
+        int t2 = 1;
+        int t3 = 1;
+        int current = 0;
+        for(int i =3;i<=n;i++){
+            current = t1+t2+t3;
+            t1 = t2;
+            t2 =t3;
+            t3 = current;
         }
-        return dp[n];
+        return t3;
 
     }
 };
