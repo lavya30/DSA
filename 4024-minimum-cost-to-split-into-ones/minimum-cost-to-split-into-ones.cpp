7ed0bfc4a199e8ab0a7660c62ace2da1efcb905f
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-09-11 23:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (64.48%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     int minCost(int n) {
-        vector<int>dp(n+1,-1);
-        dp[0] = 0;
-        dp[1] = 0;
-        int cost = 0;
-
-        for(int i = 2;i<=n;i++){
-            dp[i] = 1* (i-1);
-            cost+= dp[i];
-        }
-        return cost;
+       return n*(n-1)/2;
 
     }
 };
