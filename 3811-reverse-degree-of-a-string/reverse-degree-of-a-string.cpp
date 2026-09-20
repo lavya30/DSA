7588aed6799adb5dedadbf80177410dce003f437
Lavya Goel <155524014+lavya30@users.noreply.github.com>
2026-09-20 11:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (95.77%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int sum =0;
-        int product = 0;
-        for(int i= 0;i<size(s);i++){
-            int val = abs((s[i] - 'z'))+1;
-            product= val*(i+1);
-            sum+=product;
+
+        int sum = 0;
+
+        for(int i = 1;i<=size(s);i++){
+            char element = s[i-1];
+            int num = i* (('z' - element)+1);
+            sum+=num;
+            
         }
         return sum;
         
